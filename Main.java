fe363fc06033efcffd5220f5d4dff6c8052780cd
marcos <marcosobiang98@gmail.com>
2024-10-17 11:05:33 +0200

    Cambios en el Main

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main{
+
+public static void main(String [] args){
+	
+String [] arrayLetras= new String [10]
+try{
+for(int i=0;i<args.length;i++){ 
+System.out.print(args[i])
+
+}
+
+}
+
+catch(IndexOutOfBoundsException indexOutOfBoundsException){
+
+System.out.print("El indice del ha superado la capacidad inicial del array");
+
+}
+
+
+
+}
+
+}
